38b6b2621c452990d230eb587bd4f1d870f3ae1c
Jake Wang <delight.wjk@gmail.com>
2016-03-17 08:31:02 +0800

    Use GWT RegExp instead in HeadingParser

--- marknode/src/main/java/org/marknode/internal/HeadingParser.java
+++ marknode/src/main/java/org/marknode/internal/HeadingParser.java
@@ -1,18 +1,23 @@
 package org.marknode.internal;
 
+import com.google.gwt.regexp.shared.MatchResult;
+import com.google.gwt.regexp.shared.RegExp;
+
 import org.marknode.node.Block;
 import org.marknode.node.Heading;
 import org.marknode.parser.InlineParser;
-import org.marknode.parser.block.*;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.marknode.parser.block.AbstractBlockParser;
+import org.marknode.parser.block.AbstractBlockParserFactory;
+import org.marknode.parser.block.BlockContinue;
+import org.marknode.parser.block.BlockStart;
+import org.marknode.parser.block.MatchedBlockParser;
+import org.marknode.parser.block.ParserState;
 
 public class HeadingParser extends AbstractBlockParser {
 
-  private static Pattern ATX_HEADING = Pattern.compile("^#{1,6}(?: +|$)");
-  private static Pattern ATX_TRAILING = Pattern.compile("(^| ) *#+ *$");
-  private static Pattern SETEXT_HEADING = Pattern.compile("^(?:=+|-+) *$");
+  private static RegExp ATX_HEADING = RegExp.compile("^#{1,6}(?: +|$)");
+  private static RegExp ATX_TRAILING = RegExp.compile("(^| ) *#+ *$");
+  private static RegExp SETEXT_HEADING = RegExp.compile("^(?:=+|-+) *$");
 
   private final Heading block = new Heading();
   private final String content;
@@ -49,19 +54,20 @@ public class HeadingParser extends AbstractBlockParser {
       CharSequence line = state.getLine();
       int nextNonSpace = state.getNextNonSpaceIndex();
       CharSequence paragraph = matchedBlockParser.getParagraphContent();
-      Matcher matcher;
-      if ((matcher = ATX_HEADING.matcher(line.subSequence(nextNonSpace, line.length()))).find()) {
+      MatchResult matcher;
+      if ((matcher = ATX_HEADING.exec(
+          line.subSequence(nextNonSpace, line.length()).toString())) != null) {
         // ATX heading
-        int newOffset = nextNonSpace + matcher.group(0).length();
-        int level = matcher.group(0).trim().length(); // number of #s
+        int newOffset = nextNonSpace + matcher.getGroup(0).length();
+        int level = matcher.getGroup(0).trim().length(); // number of #s
         // remove trailing ###s:
-        String content = ATX_TRAILING.matcher(
-            line.subSequence(newOffset, line.length())).replaceAll("");
+        String content = line.subSequence(newOffset, line.length()).toString()
+            .replaceAll(ATX_TRAILING.getSource(), "");
         return BlockStart.of(new HeadingParser(level, content)).atIndex(line.length());
-      } else if (paragraph != null && ((matcher = SETEXT_HEADING.matcher(
-          line.subSequence(nextNonSpace, line.length()))).find())) {
+      } else if (paragraph != null && ((matcher = SETEXT_HEADING.exec(
+          line.subSequence(nextNonSpace, line.length()).toString())) != null)) {
         // setext heading line
-        int level = matcher.group(0).charAt(0) == '=' ? 1 : 2;
+        int level = matcher.getGroup(0).charAt(0) == '=' ? 1 : 2;
         String content = paragraph.toString();
         return BlockStart.of(new HeadingParser(level, content))
             .atIndex(line.length())
